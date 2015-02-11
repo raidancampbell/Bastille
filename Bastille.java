6c82a648fe98b0ecbc5bd33e34e155059bdf205b
R. Aidan Campbell <aidan.campbell@gmail.com>
2015-02-11 16:12:51 -0500

    updated regex

--- Bastille.java
+++ Bastille.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by aidan on 2/11/15.
@@ -49,9 +51,11 @@ public class Bastille {
      */
     public static String[] trimResults(String[] input){
         ArrayList<String> returnVar = new ArrayList<String>();
+        Pattern pattern = Pattern.compile("(tmp\\W)");
         for(String s: input){
             //valid is tmp, followed by 12 non-whitespace characters
-            if(s.matches("tmp\\W")) returnVar.add(s);
+            Matcher matcher = pattern.matcher(s);
+            if(matcher.find()) returnVar.add(matcher.group(1));
         }
         return returnVar.toArray(new String[returnVar.size()]);
     }
